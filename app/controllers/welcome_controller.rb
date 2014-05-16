dd2c5d3034fff7aea12dfb5cd9170752856149ba
Jason Ramirez <jason@thoughtbot.com>
2014-05-16 11:41:02 -0400

    Add instagram initializer, and display on root

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @posts = Post.all
+    @instagram = Instagram.user_recent_media('21684644', {:count => 1})
   end
 end
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,6 @@
+require "instagram"
+
+Instagram.configure do |config|
+  config.client_id = "d5ef5a7b662e431ab9c1799a47b39f41"
+  config.access_token = "21684644.d5ef5a7.9bc5f0cc020e4af3be5ad946458fe07f"
+end
