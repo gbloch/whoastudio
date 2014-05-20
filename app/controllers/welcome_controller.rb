92a5db2d3c3ddf80cb03ee3741198adfc3efae9b
Jason Ramirez <jason@weatetheweb.com>
2014-05-19 23:42:52 -0400

    Add welcome page and update styles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @posts = Post.all
-    @instagram = Instagram.user_recent_media('21684644', {:count => 1})
+    @instagram = latest_instagram_image
+  end
+
+  private
+
+  def latest_instagram_image
+    image = Instagram.user_recent_media('21684644', {:count => 1})
+    image[0].images.standard_resolution.url
   end
 end
