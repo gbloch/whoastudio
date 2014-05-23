1ef6cea35e3e4ca84770d5b1181cb5ebfc87a831
Jason Ramirez <jason@thoughtbot.com>
2014-05-23 16:21:36 -0400

    Style welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
   private
 
   def latest_instagram_image
-    image = Instagram.user_recent_media('21684644', {:count => 1})
+    image = Instagram.user_recent_media('1353355846', {:count => 1})
     image[0].images.standard_resolution.url
   end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 require "instagram"
 
 Instagram.configure do |config|
-  config.client_id = "d5ef5a7b662e431ab9c1799a47b39f41"
-  config.access_token = "21684644.d5ef5a7.9bc5f0cc020e4af3be5ad946458fe07f"
+  config.client_id = "698f3cc9380e4fbeb57c35f9cbaa72cb"
+  config.access_token = "1353355846.698f3cc.de73e0cbfbd649efb2c505d1a9439a13"
 end
