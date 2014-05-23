c4d22f04e27b530da63417c0c50bbe0c31de0812
Jason Ramirez <jason@thoughtbot.com>
2014-05-23 14:42:43 -0400

    Update gems

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   resources :posts, only: [:show]
 
   namespace :admin do
-    
+
     root to: "sessions#new"
-    
+
     get "sign_in", to: "sessions#new"
-    
+
     resources :posts
   end
 end
