ff2c50452c9c4790db5bfcfaf0efe4d39c2b5deb
Jason Ramirez <ramirez676@gmail.com>
2015-01-25 21:07:02 -0500

    Add admin.scss and admin.js to precomipiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  Rails.application.config.assets.precompile += %w( admin.js admin.css)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
