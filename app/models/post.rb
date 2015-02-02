4f16747a5c00399e468e334fc39525c6089beffd
Jason Ramirez <ramirez676@gmail.com>
2015-02-02 00:11:29 -0500

    Add dropbox as a bucket for assets

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
   has_attached_file(
     :feature_image,
     styles: { medium: "300x300>" },
-    default_url: "/images/:style/missing.png"
+    default_url: "/images/:style/missing.png",
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
   )
   validates_attachment_content_type(
     :feature_image,
