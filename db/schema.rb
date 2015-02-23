e1740ea05bcb9e098348ef4edf1c77e8e2b6e877
Jason Ramirez <ramirez676@gmail.com>
2015-02-22 19:07:37 -0500

    Add first name and last name to Admin model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222030320) do
+ActiveRecord::Schema.define(version: 20150222235313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20150222030320) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "approved",               default: false, null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "admins", ["approved"], name: "index_admins_on_approved", using: :btree
