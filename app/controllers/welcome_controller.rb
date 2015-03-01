363256c86b5fcf833fa068a8a84debdd411c4566
Jason Ramirez <ramirez676@gmail.com>
2015-03-01 14:58:52 -0500

    Order posts on welcome page by desc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.where(published: true).order("published_at ASC")
+    @posts = Post.where(published: true).order(published_at: :desc)
   end
 end
