cc314d9155614cbc8d5015631b40cc32ab6528be
Jason Ramirez <ramirez676@gmail.com>
2015-03-02 00:09:54 -0500

    Remove font-awesone, high_voltage and instagram gems
    * remove config/initializers/instagram.rb

--- config/initializers/instagram.rb
+++
@@ -1,6 +0,0 @@
-require "instagram"
-
-Instagram.configure do |config|
-  config.client_id = "698f3cc9380e4fbeb57c35f9cbaa72cb"
-  config.access_token = "1353355846.698f3cc.de73e0cbfbd649efb2c505d1a9439a13"
-end
