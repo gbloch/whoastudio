6a03bc68a204f0dd6ec3c471ea654c76a461c8dd
Jason Ramirez <ramirez676@gmail.com>
2015-10-22 00:13:01 -0400

    Update gravatar styles

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,4 +1,12 @@
 module PostHelper
+  def post_author_gravatar(post)
+    if(post.admin_email)
+      content_tag :div, class: "gravatar" do
+        gravatar_tag post.admin_email
+      end
+    end
+  end
+
   def post_author_full_name(post, class_name)
     if(post.author_full_name)
       content_tag :div, class: class_name do
